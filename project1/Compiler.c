3daa7b74a62488caeba57a3ffc126f81a29577e1
Alex Tang <alex.m.tang@gmail.com>
2014-02-21 13:18:43 -0500

    Finish recursive descent parser LL(1)

--- project1/Compiler.c
+++ project1/Compiler.c
@@ -21,9 +21,9 @@
      EXPR ::= + EXPR EXPR |
               - EXPR EXPR |
               * EXPR EXPR |
-              VARIABLE | 
+              VARIABLE |
               DIGIT
-     VARIABLE ::= a | b | c | d | e 
+     VARIABLE ::= a | b | c | d | e
      DIGIT ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 
      NOTE: tokens are exactly a single character long
@@ -55,9 +55,9 @@
 #define token *buffer
 
 /* GLOBALS */
-static char *buffer = NULL;	/* read buffer */
-static int regnum = 1;		/* for next free virtual register number */
-static FILE *outfile = NULL;	/* output of code generation */
+static char *buffer = NULL; /* read buffer */
+static int regnum = 1;    /* for next free virtual register number */
+static FILE *outfile = NULL;  /* output of code generation */
 
 /* Utilities */
 static void CodeGen(OpCode opcode, int field1, int field2, int field3);
@@ -85,123 +85,204 @@ static int digit();
 /*************************************************************************/
 static int digit()
 {
-	int reg;
+  int reg;
 
-	if (!is_digit(token)) {
-		ERROR("Expected digit\n");
-		exit(EXIT_FAILURE);
-	}
-	reg = next_register();
-	CodeGen(LOADI, reg, to_digit(token), EMPTY_FIELD);
-	next_token();
-	return reg;
+  if (!is_digit(token)) {
+    ERROR("Expected digit\n");
+    exit(EXIT_FAILURE);
+  }
+  reg = next_register();
+  CodeGen(LOADI, reg, to_digit(token), EMPTY_FIELD);
+  next_token();
+  return reg;
 }
 
 static int variable()  /* only called for R-values */
 {
-        int reg;
-		/* YOUR CODE GOES HERE */
-        return reg;
+  int reg;
+  /* YOUR CODE GOES HERE */
+
+  if (!is_identifier(token)) {
+    ERROR("Variable %c unknown\n", token);
+    exit(EXIT_FAILURE);
+  }
+  reg = next_register();
+  CodeGen(LOADI, reg, token, EMPTY_FIELD);
+  next_token();
+  return reg;
 }
 
 static int expr()
 {
-	int reg, left_reg, right_reg;
+  int reg, left_reg, right_reg;
 
-	switch (token) {
-	case '+':
-		next_token();
-		left_reg = expr();
-		right_reg = expr();
-		reg = next_register();
-		CodeGen(ADD, reg, left_reg, right_reg);
-		return reg;
-		/* YOUR CODE GOES HERE */
-	case '0':
-	case '1':
-	case '2':
-	case '3':
-	case '4':
-	case '5':
-	case '6':
-	case '7':
-	case '8':
-	case '9':
-		return digit();
-	default:
-		ERROR("Symbol %c unknown\n", token);
-		exit(EXIT_FAILURE);
-	}
+  switch (token) {
+  /* YOUR CODE GOES HERE */
+  case '+':
+    next_token();
+    left_reg = expr();
+    right_reg = expr();
+    reg = next_register();
+    CodeGen(ADD, reg, left_reg, right_reg);
+    return reg;
+  case '-':
+    next_token();
+    left_reg = expr();
+    right_reg = expr();
+    reg = next_register();
+    CodeGen(SUB, reg, left_reg, right_reg);
+    return reg;
+  case '*':
+    next_token();
+    left_reg = expr();
+    right_reg = expr();
+    reg = next_register();
+    CodeGen(MUL, reg, left_reg, right_reg);
+    return reg;
+  case 'a':
+  case 'b':
+  case 'c':
+  case 'd':
+  case 'e':
+    return variable();
+  case '0':
+  case '1':
+  case '2':
+  case '3':
+  case '4':
+  case '5':
+  case '6':
+  case '7':
+  case '8':
+  case '9':
+    return digit();
+  default:
+    ERROR("Symbol %c unknown\n", token);
+    exit(EXIT_FAILURE);
+  }
 }
 
 static void assign()
 {
-	char ident;
-        int reg;
+  char ident;
+  int reg;
 
-	if (!is_identifier(token)) {
-		ERROR("Expected identifier\n");
-		exit(EXIT_FAILURE);
-	}
-	ident = token;
-	next_token();
-	if (token != '=') {
-		ERROR("Expected equal sign for assignment statement\n");
-		exit(EXIT_FAILURE);
-	};
-	next_token();
-        reg = expr();
-	
-	CodeGen(STORE, ident, reg, EMPTY_FIELD);
+  if (!is_identifier(token)) {
+    ERROR("Expected identifier\n");
+    exit(EXIT_FAILURE);
+  }
+  ident = token;
+  next_token();
+  if (token != '=') {
+    ERROR("Expected equal sign for assignment statement\n");
+    exit(EXIT_FAILURE);
+  };
+  next_token();
+  reg = expr();
+
+  CodeGen(STORE, ident, reg, EMPTY_FIELD);
 }
 
 static void read()
 {
-	/* YOUR CODE GOES HERE */
+  /* YOUR CODE GOES HERE */
+  if (token != '?') {
+    ERROR("Expected read statement\n");
+    exit(EXIT_FAILURE);
+  }
+  next_token();
+  if (!is_identifier(token)) {
+    ERROR("Expected identifier\n");
+    exit(EXIT_FAILURE);
+  }
+  CodeGen(READ, token, EMPTY_FIELD, EMPTY_FIELD);
+  next_token();
 }
 
 static void print()  /* variables are handled explicitly without recursive call */
 {
-	if (token != '!') {
-		ERROR("Expected print statement\n");
-		exit(EXIT_FAILURE);
-	}
-	next_token();
-	if (!is_identifier(token)) {
-		ERROR("Expected identifier\n");
-		exit(EXIT_FAILURE);
-	}
-	CodeGen(WRITE, token, EMPTY_FIELD, EMPTY_FIELD);
-	next_token();
+  if (token != '!') {
+    ERROR("Expected print statement\n");
+    exit(EXIT_FAILURE);
+  }
+  next_token();
+  if (!is_identifier(token)) {
+    ERROR("Expected identifier\n");
+    exit(EXIT_FAILURE);
+  }
+  CodeGen(WRITE, token, EMPTY_FIELD, EMPTY_FIELD);
+  next_token();
 }
 
 static void stmt()
 {
-	/* YOUR CODE GOES HERE */
+  /* YOUR CODE GOES HERE */
+  switch (token) {
+  case 'a':
+  case 'b':
+  case 'c':
+  case 'd':
+  case 'e':
+    assign();
+    break;
+  case '?':
+    read();
+    break;
+  case '!':
+    print();
+    break;
+  default:
+    ERROR("Invalid statement\n");
+    exit(EXIT_FAILURE);
+  }
 }
 
 static void morestmts()
 {
-	/* YOUR CODE GOES HERE */
+  /* YOUR CODE GOES HERE */
+  switch (token) {
+  case ';':
+    next_token();
+    stmtlist();
+    break;
+  case '.':
+    return;
+    break;
+  default:
+    ERROR("Invalid more statements\n");
+    exit(EXIT_FAILURE);
+  }
 }
 
 static void stmtlist()
 {
-	/* YOUR CODE GOES HERE */
+  /* YOUR CODE GOES HERE */
+  switch (token) {
+  case 'a':
+  case 'b':
+  case 'c':
+  case 'd':
+  case 'e':
+  case '?':
+  case '!':
+    stmt();
+    morestmts();
+    break;
+  default:
+    ERROR("Invalid statement list\n");
+    exit(EXIT_FAILURE);
+  }
 }
 
 static void program()
 {
-        assign();     /* DUMMY - NEEDS TO BE DELETED */
-        next_token(); /* DUMMY - NEEDS TO BE DELETED */
-        print();      /* DUMMY - NEEDS TO BE DELETED */
+  /* YOUR CODE GOES HERE */
+  stmtlist();
 
-	/* YOUR CODE GOES HERE */
-
-	if (token != '.') {
-		ERROR("Program error.  Current input symbol is %c\n", token);
-		exit(EXIT_FAILURE);
-	};
+  if (token != '.') {
+    ERROR("Program error.  Current input symbol is %c\n", token);
+    exit(EXIT_FAILURE);
+  };
 }
 
 /*************************************************************************/
@@ -209,90 +290,90 @@ static void program()
 /*************************************************************************/
 static void CodeGen(OpCode opcode, int field1, int field2, int field3)
 {
-	Instruction instr;
+  Instruction instr;
 
-	if (!outfile) {
-		ERROR("File error\n");
-		exit(EXIT_FAILURE);
-	}
-	instr.opcode = opcode;
-	instr.field1 = field1;
-	instr.field2 = field2;
-	instr.field3 = field3;
-	PrintInstruction(outfile, &instr);
+  if (!outfile) {
+    ERROR("File error\n");
+    exit(EXIT_FAILURE);
+  }
+  instr.opcode = opcode;
+  instr.field1 = field1;
+  instr.field2 = field2;
+  instr.field3 = field3;
+  PrintInstruction(outfile, &instr);
 }
 
 static inline void next_token()
 {
-	if (*buffer == '\0') {
-		ERROR("End of program input\n");
-		exit(EXIT_FAILURE);
-	}
-	printf("%c ", *buffer);
-	if (*buffer == ';')
-		printf("\n");
-	buffer++;
-	if (*buffer == '\0') {
-		ERROR("End of program input\n");
-		exit(EXIT_FAILURE);
-	}
-	if (*buffer == '.')
-		printf(".\n");
+  if (*buffer == '\0') {
+    ERROR("End of program input\n");
+    exit(EXIT_FAILURE);
+  }
+  printf("%c ", *buffer);
+  if (*buffer == ';')
+    printf("\n");
+  buffer++;
+  if (*buffer == '\0') {
+    ERROR("End of program input\n");
+    exit(EXIT_FAILURE);
+  }
+  if (*buffer == '.')
+    printf(".\n");
 }
 
 static inline int next_register()
 {
-	return regnum++;
+  return regnum++;
 }
 
 static inline int is_digit(char c)
 {
-	if (c >= '0' && c <= '9')
-		return 1;
-	return 0;
+  if (c >= '0' && c <= '9')
+    return 1;
+  return 0;
 }
 
 static inline int to_digit(char c)
 {
-	if (is_digit(c))
-		return c - '0';
-	WARNING("Non-digit passed to %s, returning zero\n", __func__);
-	return 0;
+  if (is_digit(c))
+    return c - '0';
+  WARNING("Non-digit passed to %s, returning zero\n", __func__);
+  return 0;
 }
 
 static inline int is_identifier(char c)
 {
-	if (c >= 'a' && c <= 'e')
-		return 1;
-	return 0;
+  if (c >= 'a' && c <= 'e')
+    return 1;
+  return 0;
 }
 
 static char *read_input(FILE * f)
 {
-	size_t size, i;
-	char *b;
-	int c;
+  size_t size, i;
+  char *b;
+  int c;
 
-	for (b = NULL, size = 0, i = 0;;) {
-		if (i >= size) {
-			size = (size == 0) ? MAX_BUFFER_SIZE : size * 2;
-			b = (char *)realloc(b, size * sizeof(char));
-			if (!b) {
-				ERROR("Realloc failed\n");
-				exit(EXIT_FAILURE);
-			}
-		}
-		c = fgetc(f);
-		if (EOF == c) {
-			b[i] = '\0';
-			break;
-		}
-		if (isspace(c))
-			continue;
-		b[i] = c;
-		i++;
-	}
-	return b;
+  for (b = NULL, size = 0, i = 0;;) {
+    if (i >= size) {
+      size = (size == 0) ? MAX_BUFFER_SIZE : size * 2;
+      b = (char *)realloc(b, size * sizeof(char));
+      if (!b) {
+        ERROR("Realloc failed\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+    c = fgetc(f);
+    if (EOF == c) {
+      b[i] = '\0';
+      break;
+    }
+    if (isspace(c))
+      continue;
+    b[i] = c;
+    i++;
+  }
+  return b;
 }
 
 /*************************************************************************/
@@ -301,33 +382,33 @@ static char *read_input(FILE * f)
 
 int main(int argc, char *argv[])
 {
-	const char *outfilename = "tinyL.out";
-	char *input;
-	FILE *infile;
+  const char *outfilename = "tinyL.out";
+  char *input;
+  FILE *infile;
 
-	printf("------------------------------------------------\n");
-	printf("CS314 compiler for tinyL\n      Spring 2014\n");
-	printf("------------------------------------------------\n");
-	if (argc != 2) {
-		ERROR("Use of command:\n  compile <tinyL file>\n");
-		exit(EXIT_FAILURE);
-	}
-	infile = fopen(argv[1], "r");
-	if (!infile) {
-		ERROR("Cannot open input file \"%s\"\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	outfile = fopen(outfilename, "w");
-	if (!outfile) {
-		ERROR("Cannot open output file \"%s\"\n", outfilename);
-		exit(EXIT_FAILURE);
-	}
-	input = read_input(infile);
-	buffer = input;
-	program();
-	printf("\nCode written to file \"%s\".\n\n", outfilename);
-	free(input);
-	fclose(infile);
-	fclose(outfile);
-	return EXIT_SUCCESS;
+  printf("------------------------------------------------\n");
+  printf("CS314 compiler for tinyL\n      Spring 2014\n");
+  printf("------------------------------------------------\n");
+  if (argc != 2) {
+    ERROR("Use of command:\n  compile <tinyL file>\n");
+    exit(EXIT_FAILURE);
+  }
+  infile = fopen(argv[1], "r");
+  if (!infile) {
+    ERROR("Cannot open input file \"%s\"\n", argv[1]);
+    exit(EXIT_FAILURE);
+  }
+  outfile = fopen(outfilename, "w");
+  if (!outfile) {
+    ERROR("Cannot open output file \"%s\"\n", outfilename);
+    exit(EXIT_FAILURE);
+  }
+  input = read_input(infile);
+  buffer = input;
+  program();
+  printf("\nCode written to file \"%s\".\n\n", outfilename);
+  free(input);
+  fclose(infile);
+  fclose(outfile);
+  return EXIT_SUCCESS;
 }
